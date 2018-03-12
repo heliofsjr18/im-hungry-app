59ac0563f36b349dda571587d45cf8b8c59c9b65
Ismael Gomes <ismaelgomes800@gmail.com>
2018-03-12 17:15:49 -0300

    Adição de barra de pesquisa na tela de listagem de estabelecimentos, e adicionado referência do jQuery para fazer manipulação de elementos DOM

--- ImHungryApp/src/app/app.module.ts
+++ ImHungryApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -29,6 +30,7 @@ import { IonRatingComponent } from '../components/ion-rating/ion-rating';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- ImHungryApp/src/pages/estabelecimento-list/estabelecimento-list.ts
+++ ImHungryApp/src/pages/estabelecimento-list/estabelecimento-list.ts
@@ -1,15 +1,30 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { style, state, animate, transition, trigger } from '@angular/animations';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MenuListPage } from '../menu-list/menu-list';
+import $ from "jquery";
 
 @IonicPage()
 @Component({
   selector: 'page-estabelecimento-list',
   templateUrl: 'estabelecimento-list.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)', opacity: 0}),
+        animate('150ms', style({transform: 'translateX(0)', opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({transform: 'translateX(0)', opacity: 1}),
+        animate('150ms', style({transform: 'translateX(100%)', opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class EstabelecimentoListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private detector: ChangeDetectorRef) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private detector: ChangeDetectorRef,
+  private elRef: ElementRef) {
   }
 
   data = [
@@ -20,8 +35,31 @@ export class EstabelecimentoListPage {
     {name: 'Pizza Hut', description: 'Pizza Hut da Favela', image: '/assets/imgs/test-logo.png', rate: 5}
   ];
 
+  showSearch: boolean = false;
+
+  toggleSearch(){
+    this.showSearch = !this.showSearch;
+  }
+
+  clickSearch(){
+    this.toggleSearch();
+    
+    setTimeout(()=>{
+      $(".searchbar-ios-cancel > .button-inner").text("Cancelar");
+    },200);
+    
+  }
+
+  onCancel(event){
+    this.toggleSearch();
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EstabelecimentoListPage');
+    
+  }
+
+  navigateToMenuPage(){
+    this.navCtrl.push(MenuListPage);
   }
 
   onScroll(){
@@ -31,9 +69,4 @@ export class EstabelecimentoListPage {
   logStars(event){
     console.log(event);
   }
-
-  chamaTelaMenu() {
-    this.navCtrl.push(MenuListPage);
-  }
-
 }
