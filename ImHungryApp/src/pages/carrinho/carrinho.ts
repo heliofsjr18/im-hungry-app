def6dd25813ad88a53dc45bc4cce45d2d4de68d8
Helio Ferreira <heliofsjr18@hotmail.com>
2018-03-20 13:35:15 -0300

    Deixando a tela de Carrinho Dinamica

--- ImHungryApp/src/pages/carrinho/carrinho.ts
+++ ImHungryApp/src/pages/carrinho/carrinho.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform, LoadingController } from 'ionic-angular';
 import $ from "jquery";
 
@@ -8,6 +8,14 @@ import $ from "jquery";
   templateUrl: 'carrinho.html',
 })
 export class CarrinhoPage {
+
+  @ViewChild('lblMultiplicador') lblMultiplicadorRef: ElementRef;
+  @ViewChild('lblDescricao') lblDescricaoRef: ElementRef;
+  @ViewChild('lblPreco') lblPrecoRef: ElementRef;
+
+  someWhereInCode() {
+    console.log(this.mylblRef.nativeElement.innerText);
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private platform: Platform,
    private loadCtrl: LoadingController) {
@@ -33,5 +41,15 @@ export class CarrinhoPage {
       loading.dismiss();
     },10000);
   }
+
+
+  addButton() {
+    this.lblMultiplicadorRef.nativeElement.innerText =+ 1;
+  }
+
+  removeButton(){
+    this.lblMultiplicadorRef.nativeElement.innerText =- 1;
+  }
+
 
 }
