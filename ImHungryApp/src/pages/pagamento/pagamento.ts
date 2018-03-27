c2a27b798166fe1d9de48d415f5a4dacfd060c43
Ismael Gomes <ismaelgomes800@gmail.com>
2018-03-27 02:23:49 -0300

    Coreção do layout da tela de pagamento

--- ImHungryApp/src/pages/pagamento/pagamento.ts
+++ ImHungryApp/src/pages/pagamento/pagamento.ts
@@ -9,19 +9,29 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 })
 export class PagamentoPage {
 
-  @ViewChild(Slides) slides: Slides;
+  @ViewChild('firstSlider') slides: Slides;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  meses = [{name: "Jan"}, {name: "Fev"}, {name: "Mar"}, {name: "Abr"},
+  {name: "Mai"}, {name: "Jun"}, {name: "Jul"}, {name: "Ago"},
+  {name: "Set"}, {name: "Out"}, {name: "Nov"}, {name: "Dez"}];
+
+  anos = [{num: 2018}, {num: 2019}, {num: 2020}, {num: 2021},
+    {num: 2022}, {num: 2023}, {num: 2024}, {num: 2025}];
+
+  selectedCard = {};
+
   ionViewDidLoad() {
-    this.slides.lockSwipes(true);
+    this.slides.loop = true;
+    this.slides.lockSwipes(false);
   }
 
   closeModal(){
     this.navCtrl.pop();
   }
 
-  setNext(isPayment: boolean){
+  setNext(){
     this.slides.lockSwipes(false);
     this.slides.slideNext();
     this.slides.lockSwipes(true);
