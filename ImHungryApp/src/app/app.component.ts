0512339a182f2d81fd9bbff300829b53f193303b
Ismael Gomes <ismaelgomes800@gmail.com>
2018-04-05 16:47:32 -0300

    O menu de listagem de estabelecimentos agora mostra duas opções de filtragem (ainda não se comunica com a api)

--- ImHungryApp/src/app/app.component.ts
+++ ImHungryApp/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 import { HomePage } from '../pages/home/home';
 import { EstabelecimentoListPage } from '../pages/estabelecimento-list/estabelecimento-list';
@@ -12,13 +13,18 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  closeMenu(id: string){
+    this.menuCtrl.get(id).close();
   }
 }
 
